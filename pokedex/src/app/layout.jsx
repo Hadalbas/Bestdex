278067f19b22bfb6e271dd4a9fb0df719d85f454
OrlindoMene <cadud.benedetto@gmail.com>
2026-09-17 10:46:31 -0300

    stats page design

--- pokedex/src/app/layout.jsx
+++ pokedex/src/app/layout.jsx
@@ -1,5 +1,8 @@
 import Link from "next/link";
+import { Montserrat } from "next/font/google";
 import "./globals.css";
+
+const montserrat = Montserrat({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Bestdex",
@@ -8,7 +11,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={montserrat.className}>
 
       <head>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.3.1/css/all.min.css" integrity="sha512-QeR2VH+lsBE5LSAe1Q5EnTBbe7XTBubt8dG93Y7gidSgdMCr8nVqKcfKAMyN96SV8KDbZVTDXChatu5G2KQGzg==" crossOrigin="anonymous" referrerPolicy="no-referrer"></link>
