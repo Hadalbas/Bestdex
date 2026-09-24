2dfcb65caa68837d95fa60a13c56b7c885fa25e0
OrlindoMene <cadud.benedetto@gmail.com>
2026-09-24 11:03:43 -0300

    Added Accordion and UX Design adjustments

--- pokedex/src/app/[id]/page.jsx
+++ pokedex/src/app/[id]/page.jsx
@@ -89,7 +89,6 @@ export default function Pokemon({ params }) {
                             <h1>{pokemon.name}</h1>
                         </div>
                     </div>
-                    <div className="stats-abilities-container">
                         <div className="stats-container">
                             <h2>Base stats</h2>
                             <div className="stats">
@@ -120,8 +119,6 @@ export default function Pokemon({ params }) {
                             </div>
                         </div>
 
-                        <br />
-
                         <div className="abilities">
                             <h2>Abilities</h2>
                             <h4>Normal: {pokemon?.abilities ? pokemon?.abilities["0"] : "carregando"} {(pokemon?.abilities && pokemon?.abilities["1"]) != undefined && <>{"and " + pokemon?.abilities["1"]}</>}</h4>
@@ -130,7 +127,6 @@ export default function Pokemon({ params }) {
                                 pokemon?.abilities?.H && <h4>Hidden: {pokemon?.abilities?.H}</h4>
                             }
                         </div>
-                    </div>
                 </div>
 
                 <br />
@@ -176,10 +172,24 @@ export default function Pokemon({ params }) {
                     </div>
                 </div>
 
-                <Link href="/"><button className="exibir">Back</button></Link>
+                
+                <div className="accordion" id="accordionExample">
+                    <div className="accordion-item">
+                        <h2 className="accordion-header" id="headingOne">
+                        <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                            Learn Sets
+                        </button>
+                        </h2>
+                        <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                        <div className="accordion-body">
+                            <Moves learnset={learnset} typesNumbers={typesNumbers} />
+                        </div>
+                        </div>
+                    </div>
+                    
+                </div>
 
-                <Moves learnset={learnset} typesNumbers={typesNumbers} />
-                <br />
+                <Link href="/"><button className="exibir">Back</button></Link>
 
             </div>
 
